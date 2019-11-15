8045b39192a01b7a6a5861474738b081082c37c3
Quinn Krug <quinn.krug@gmail.com>
2019-11-15 14:05:20 -0700

    add event listener to button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Steph', age: 26}
+    ]
+  }
+
+  switchNameHandler = () =>{
+    console.log('was clicked')
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi, Im a React App </h1>
         <p> this works </p>
-        <Person name ="Max" age="28">My Hobbies: Racing</Person>
-        <Person name ="Manu" age="29"/>
+        <button onClick={this.switchNameHandler}> Switch Name</button>
+        <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
+        <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name ={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Does this work?'))
