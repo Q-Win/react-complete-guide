cffbb49f0eb46b0b936b4df6bb8dcbe6a3721e17
Quinn Krug <quinn.krug@gmail.com>
2019-12-02 17:55:33 -0700

    changed to const

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Steph', age: 26}
-    ]
-  }
+const app = props => {
 
-  switchNameHandler = () =>{
-    console.log('was clicked')
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1> Hi, Im a React App </h1>
-        <p> this works </p>
-        <button onClick={this.switchNameHandler}> Switch Name</button>
-        <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
-        <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name ={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Does this work?'))
-  }
+  return (
+    <div className="App">
+      <h1> Hi, Im a React App </h1>
+      <p> this works </p>
+      <button onClick={this.switchNameHandler}> Switch Name</button>
+      <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
+      <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name ={this.state.persons[2].name} age={this.state.persons[2].age}/>
+    </div>
+  );
+  // return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Does this work?'))
 }
 
-export default App;
+export default app;
+
+state = {
+  persons: [
+    {name: 'Max', age: 28},
+    {name: 'Manu', age: 29},
+    {name: 'Steph', age: 26}
+  ]
+}
+
+switchNameHandler = () =>{
+  // Dont do this this.state.persons[0].name = "Maximilian";
+  this.setState({persons: [
+    {name: 'Maximilian', age: 28},
+    {name: 'Manu', age: 29},
+    {name: 'Steph', age: 27}
+  ]})
+}
